b9adf43e714348f098c59b344110762c1e694174
tkopp <tkopp@pixar.com>
2017-06-16 07:51:40 -0700

    test

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+const http = require('http');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.set('port', (process.env.PORT || 3001));
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.route('/books')
+.get(function (req, res) {
+  MongoClient.connect('mongodb://localhost:27017/library', function (err, db) {
+    if (err) throw err
+
+    db.collection('books').find().toArray(function (err, result) {
+      if (err) throw err
+      res.send(result)
+      console.log('Get on port 3000');
+    })
+  })
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
